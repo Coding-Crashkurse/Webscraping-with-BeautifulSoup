9034c60ace8c59e163c1f84a6a5da3fe9a0fb324
markus <datamastery87@gmail.com>
2021-04-11 08:45:27 +0200

    updated code

--- script.py
+++ script.py
@@ -1,46 +1,51 @@
 import requests
-import csv
+# pip install beautifulsoup4
 from bs4 import BeautifulSoup
 
-
-
-url = "https://www.transfermarkt.de/1-bundesliga/marktwerte/wettbewerb/L1/ajax/yw1/page/1"
+url = "https://www.transfermarkt.de/1-bundesliga/marktwerte/wettbewerb/L1/ajax/ywi/page/1"
 heads = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
+
+
 response = requests.get(url, headers=heads)
-response.text
 html = response.text
 soup = BeautifulSoup(html, "html.parser")
-elem = soup.find(id="yw1")
-footballers = elem.find_all("a",{"class":"spielprofil_tooltip"})
+
+table = soup.find(id="yw1")
+footballers = table.find_all("a", {"class": "spielprofil_tooltip"})
 
 footballers[0].text
 
-prices = elem.find_all("td",{"class":"rechts hauptlink"})
-float(prices[0].text.split()[0].replace(",", "."))
+all_players = [player.text for player in footballers]
 
-### Final solution
+
+prices = table.find_all("td", {"class": "rechts hauptlink"})
+[float(price.text.split()[0].replace(",", ".")) for price in prices]
+
+##########################
+heads = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
+
 pages = list(range(1, 5))
+pages
+
 footballer_list = []
 price_list = []
 
 for page in pages:
-    url = "https://www.transfermarkt.de/1-bundesliga/marktwerte/wettbewerb/L1/ajax/yw1/page/" + str(page)
-    heads = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
+    url = "https://www.transfermarkt.de/1-bundesliga/marktwerte/wettbewerb/L1/ajax/ywi/page/" + str(page)
     response = requests.get(url, headers=heads)
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
-    elem = soup.find(id="yw1")
-    footballers = elem.find_all("a",{"class":"spielprofil_tooltip"})
-    prices = elem.find_all("td",{"class":"rechts hauptlink"})
+    table = soup.find(id="yw1")
+    footballers = table.find_all("a", {"class": "spielprofil_tooltip"})
+    all_players = [player.text for player in footballers]
+    footballer_list.extend(all_players)
     
-    for footballer in footballers:
-        footballer_list.append(footballer.text.strip())
-        
-    for price in prices:
-        price_list.append(float(price.text.split()[0].replace(",", ".")))
-        
-footballer_list
-price_list  
+    prices = table.find_all("td", {"class": "rechts hauptlink"})
+    all_prices = [float(price.text.split()[0].replace(",", ".")) for price in prices]
+    price_list.extend(all_prices)
+    
+footballer_list   
 
-result = list(zip(footballer_list, price_list))
-result
+list(zip(footballer_list, price_list))
+    
+    
